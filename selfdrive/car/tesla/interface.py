8ad887c92769d40f8f7d6f1cfa3ba62182544476
Mart Raudsepp <mart.raudsepp@cooppank.ee>
2026-02-19 18:28:41 +0200

    LEAD_ACCEL_TAU little agressive and pid tuning

--- selfdrive/car/tesla/interface.py
+++ selfdrive/car/tesla/interface.py
@@ -19,9 +19,9 @@ class CarInterface(CarInterfaceBase):
     ret.steerControlType = car.CarParams.SteerControlType.angle
 
     ret.longitudinalTuning.kpBP = [0., 10., 20.]
-    ret.longitudinalTuning.kpV = [0.26, 0.22, 0.20]
+    ret.longitudinalTuning.kpV = [0.24, 0.20, 0.18]
     ret.longitudinalTuning.kiBP = [0., 10., 20.]
-    ret.longitudinalTuning.kiV = [0.32, 0.28, 0.24]
+    ret.longitudinalTuning.kiV = [0.40, 0.36, 0.28]
 
     ret.longitudinalActuatorDelay = 0.15 # s
     ret.radarUnavailable = True
